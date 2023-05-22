b10aa4797cd5a9289d53425938a2402c94aaf6e2
AldsDeveloper <119023620+AldsDeveloper@users.noreply.github.com>
2023-05-22 12:26:04 +0700

    Initail commit

--- .docker/mysql/phpmyadmin/config.user.inc.php
+++ .docker/mysql/phpmyadmin/config.user.inc.php
@@ -25,7 +25,7 @@ $i = 0;
  */
 $i++;
 /* Authentication type */
-// $cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['auth_type'] = 'config';
 // $cfg['Servers'][$i]['user'] = 'root';
 // $cfg['Servers'][$i]['password'] = 'frankent';
 
